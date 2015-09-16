b6897e01eacbbaf10f5f6ed0d1b22b566aa03664
chinanier <306379128@qq.com>
2015-09-16 22:35:43 +0800

    移动原仓库到现在的之艺琴行仓库，初始化该仓库。

---
+++ js/comm.js
@@ -0,0 +1,17 @@
+/**
+ * Created by chinanier on 2015/8/10 0010.
+ */
+
+
+/*************************************************
+ Function:		ChangeFrame
+ Description:	主页面加载时，获取cookie，跳转到刷新前的界面
+ Input:			src:页面路径
+ index:ID序号
+ Output:			无
+ return:			无
+ *************************************************/
+function ChangeFrame(e)
+{
+    alert(e.nodeType);
+}
